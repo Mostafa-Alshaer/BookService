28a35256a6c27eac3407a5e8de48142ae83a7a70
your name <you@example.com>
2019-02-12 12:01:47 +0200

    --

---
+++ BookService/AutoMapper/AutoMapperApplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using BookService.Core.Models;
+using BookService.Models;
+
+namespace BookService.AutoMapper
+{
+    public class AutoMapperApplication : Profile
+    {
+        public AutoMapperApplication()
+        {
+            CreateMap<Core.Models.Book, BookDTO>().ReverseMap();
+            CreateMap<Core.Models.Book, BookDetailDTO>().ReverseMap();
+        }
+    }
+}
